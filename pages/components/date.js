2bf8d92646fe67335b16ab87b783a2d34d587cd0
Will <willygomezr1@gmail.com>
2023-10-28 00:36:59 -0400

    trying again

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,12 @@
 import { parseISO, format } from 'date-fns';
 
+
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  try {
+    const date = parseISO(dateString);
+    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  } catch (error) {
+    console.error('Error parsing date:', dateString, error);
+    return <time>Error parsing date</time>;
+  }
 }
